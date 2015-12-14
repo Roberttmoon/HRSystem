712b4a8a01596828d78db3c9ef0229e7b2609f05
Alex Morask <alex.morask@outlook.com>
2015-12-14 11:20:38 -0600

    Added Project and Task classes

---
+++ HRSystem/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTimeEntry
+{
+    public class Task : Project
+    {
+        protected string taskName;
+        protected Guid taskID;
+
+        public Task()
+        {
+
+        }
+
+        public AddHours(int hours)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AddResource(BillableAsset asset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
