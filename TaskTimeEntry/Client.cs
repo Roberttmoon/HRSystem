185d5252b14ac72ffa44c00b62cd50c9002d8634
roberttmoon@gmail.com <roberttmoon@gmail.com>
2015-12-14 11:22:46 -0600

    added ITaskInteract and client

---
+++ TaskTimeEntry/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskTimeEntry
+{
+    class Client
+    {
+        string clientName;
+        Guid clientID;
+        List<Projects> clientProjects;
+        float clientDiscount;
+
+        void addTime(Project project, int time)
+        {
+
+        }
+    }
+}
