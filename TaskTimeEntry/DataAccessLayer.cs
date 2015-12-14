3256d14eb2bd605b7e2c7e7446f54a1bcbd2e158
roberttmoon@gmail.com <roberttmoon@gmail.com>
2015-12-14 11:54:55 -0600

    added DataAccessLayer

---
+++ TaskTimeEntry/DataAccessLayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRSystem
+{
+    class DataAccessLayer
+    {
+        string path;
+
+        int getProjectHoursActual(Guid projectID)
+        {
+
+            return 0;
+        }
+        //ResourceReport getResourceReport(BillableAsset asset){}
+        void updateTask(Task task)
+        {
+
+        }
+    }
+}
