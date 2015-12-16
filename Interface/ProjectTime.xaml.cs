025feb9822681f7bc5a2f34c04a55fc560e0498c
Alex Morask <alex.morask@outlook.com>
2015-12-16 13:33:40 -0600

    Fixed Project to Client Add

--- Interface/ProjectTime.xaml.cs
+++ Interface/ProjectTime.xaml.cs
@@ -41,7 +41,6 @@ namespace Interface
             Client client = (Client)ChooseClient.SelectedItem;
             project.clientID = client._id;
             client.AddProject(project);
-            client.name = "NAME CHANGED";
             ModelView.ReplaceClient(client);
             MessageBox.Show("Success. Project added to client.");
         }
--- TaskTimeEntry/UserAccount.cs
+++ TaskTimeEntry/UserAccount.cs
@@ -12,12 +12,12 @@ namespace TaskTimeEntry
         public string name { get; set; }
         public string email { get; protected set; }
         public Guid _id { get; protected set; }
-        public List<Guid> projects { get; protected set; }        
+        public List<Project> projects { get; protected set; }        
         public List<Task> tasks { get; protected set; }
 
         public void AddProject(Project project)
         {
-            projects.Add(project._id);
+            projects.Add(project);
         }
 
         public int LogTime(Task task, float time, string comment)
